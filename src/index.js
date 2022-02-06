5ada97fd8d272232ea38b85605aa79835f5a5417
thelastandrew <thelastandrew@gmail.com>
2022-02-06 09:16:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let res = [];
+    n = Math.abs(n);
+    while (n != 0) {
+        res.push(n % 10);
+        n = Math.floor(n / 10);
+    }
+    return Number(res.join(""));
+};
